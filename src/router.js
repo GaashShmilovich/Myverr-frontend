0e7affbf9ff80bf881dfee58c0174d2fa321707b
Oryan Hachamoff <oryanhach95@gmail.com>
2023-07-25 21:41:48 +0300

    updated route addresses

--- src/router.js
+++ src/router.js
@@ -2,48 +2,46 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 
 import Home from './views/Home.vue'
 import Chat from './views/Chat.vue'
-import GigIndex from './views/GigIndex.vue'
+import GigIndex from './views/explore/GigIndex.vue'
 import ReviewIndex from './views/ReviewIndex.vue'
 import LoginSignup from './views/LoginSignup.vue'
 import UserDetails from './views/UserDetails.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/explore',
-    name: 'GigIndex',
-    component: GigIndex
-  },
-  {
-    path: '/review',
-    name: 'ReviewIndex',
-    component: ReviewIndex
-  },
-  {
-    path: '/chat',
-    name: 'Chat',
-    component: Chat
-  },
-  {
-    path: '/login',
-    name: 'LoginSignup',
-    component: LoginSignup
-  },
-  {
-    path: '/user/:id',
-    name: 'UserDetails',
-    component: UserDetails
-  }
+	{
+		path: '/',
+		name: 'Home',
+		component: Home,
+	},
+	{
+		path: '/explore',
+		name: 'GigIndex',
+		component: GigIndex,
+	},
+	{
+		path: '/review',
+		name: 'ReviewIndex',
+		component: ReviewIndex,
+	},
+	{
+		path: '/chat',
+		name: 'Chat',
+		component: Chat,
+	},
+	{
+		path: '/login',
+		name: 'LoginSignup',
+		component: LoginSignup,
+	},
+	{
+		path: '/user/:id',
+		name: 'UserDetails',
+		component: UserDetails,
+	},
 ]
 
-
 export const router = createRouter({
-  routes,
-  history: createWebHashHistory()
-  // base: process.env.BASE_URL,
+	routes,
+	history: createWebHashHistory(),
+	// base: process.env.BASE_URL,
 })
-
---
+++ src/views/explore/GigPreview.vue
@@ -0,0 +1,15 @@
+<template>
+    <router-link :to="{ name: 'GigDetails', params: { id: gig.id } }" class="gig-preview">
+      <h3>{{ gig.title }}</h3>
+      <p>{{ gig.description }}</p>
+      <p>{{ gig.price }}</p>
+    </router-link>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      gig: Object
+    }
+  }
+  </script>
