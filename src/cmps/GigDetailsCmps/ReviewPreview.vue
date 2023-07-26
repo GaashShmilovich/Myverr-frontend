18ac5d99902b300132a5ebcc1618800a20045f25
Giora Marani <giora.marani@gmail.com>
2023-07-26 18:32:20 +0300

    make reviews ui better

--- src/cmps/GigDetailsCmps/ReviewPreview.vue
+++ src/cmps/GigDetailsCmps/ReviewPreview.vue
@@ -1,16 +1,22 @@
 <template lang="">
-  <article class="m-1">
+  <section class="review-preview" >
+    <section class="review-by">
   <img :src="review.by.imgUrl" alt="">
+      <section class="review-by-name">
   <p> {{ review.by.fullname }}</p>
   <p> {{ review.by.country }}</p>
+      </section>
+    </section>
+    <section class="review-rate-time">
     <span v-for="i in review.rate || 1">
       <font-awesome-icon
         :style="{ color: '#ffb33e' }"
         icon="fa-solid fa-star"
       /> 
     </span> || <span> {{ review.createdAt }}</span>
+    </section>
     <p> {{ review.txt }}</p>
-  </article>
+  </section>
 </template>
 <script>
 export default {
