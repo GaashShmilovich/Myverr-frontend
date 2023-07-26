23e7a10e9a537777534f78913a0d3b03dbf381fd
Oryan Hachamoff <oryanhach95@gmail.com>
2023-07-26 15:11:05 +0300

    added basic gig list preview layout

--- src/views/explore/GigPreview.vue
+++ src/views/explore/GigPreview.vue
@@ -1,15 +1,44 @@
 <template>
-    <router-link :to="{ name: 'GigDetails', params: { id: gig.id } }" class="gig-preview">
-      <h3>{{ gig.title }}</h3>
-      <p>{{ gig.description }}</p>
-      <p>{{ gig.price }}</p>
-    </router-link>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      gig: Object
-    }
-  }
-  </script>
+	<div class="gig-preview-container">
+		<div class="gig-preview">
+			<div class="gig-preview__img-container">
+				<img
+					src="https://picsum.photos/330/220"
+					alt="gig.title"
+					class="gig-preview__img"
+				/>
+			</div>
+			<div class="gig-preview__info">
+				<div class="owner-preview-container">
+					<img
+						class="gig-owner-img"
+						src="https://picsum.photos/24/24"
+						alt=""
+					/>
+					<h3 class="gig-preview-owner">
+						{{ gig.owner.fullname }}<br /><span>{{
+							gig.owner.level
+						}}</span>
+					</h3>
+				</div>
+				<h4 class="gig-preview__title">{{ gig.title }}</h4>
+				<p class="gig-preview-rate">⭐{{ gig.owner.rate }}</p>
+				<hr />
+				<div class="gig-preview-footer-container">
+					<p class="gig-like-btn">❤</p>
+					<p class="gig-preview__price">
+						Starting at <br /><span>{{ gig.price }}$</span>
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		gig: Object,
+	},
+}
+</script>
