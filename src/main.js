a2f48d88423800924ca7b905ee865143af3dcc62
Gaash Shmilovich <gaash.shm@gmail.com>
2023-07-26 17:38:11 +0300

    adding element plus

--- src/main.js
+++ src/main.js
@@ -2,15 +2,16 @@ import { createApp } from 'vue'
 
 import { router } from './router.js'
 import { store } from './store/store.js'
-
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import {
-  faChartLine,
-  faChildren,
-  faHome,
-  faAddressCard,
-  faStar,
+    faChartLine,
+    faChildren,
+    faHome,
+    faAddressCard,
+    faStar,
 } from '@fortawesome/free-solid-svg-icons'
 import './assets/styles/main.scss'
 import './assets/styles/setup/_typography.scss'
@@ -23,5 +24,5 @@ app.use(router)
 app.use(store)
 
 app.component('font-awesome-icon', FontAwesomeIcon)
-
+app.use(ElementPlus)
 app.mount('#app')
