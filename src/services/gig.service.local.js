48a30068d0bc6e886f83a878cdb38a51914a7c10
Oryan Hachamoff <oryanhach95@gmail.com>
2023-07-26 11:58:30 +0300

    renderd gig list skeleton

--- src/services/gig.service.local.js
+++ src/services/gig.service.local.js
@@ -1,4 +1,3 @@
-
 import { storageService } from './async-storage.service.js'
 import { utilService } from './util.service.js'
 import { userService } from './user.service.local.js'
@@ -6,86 +5,136 @@ import { userService } from './user.service.local.js'
 const STORAGE_KEY = 'gig'
 
 export const gigService = {
-    query,
-    getById,
-    save,
-    remove,
-    getEmptyGig,
-    // addGigMsg
+	query,
+	getById,
+	save,
+	remove,
+	getEmptyGig,
+	// addGigMsg
 }
 window.cs = gigService
 
-
-async function query(filterBy = { txt: ''}) {
-    var gigs = await storageService.query(STORAGE_KEY)
-    if (filterBy.txt) {
-        const regex = new RegExp(filterBy.txt, 'i')
-        gigs = gigs.filter(gig => regex.test(gig.title) || regex.test(gig.description) || regex.test(gig.owner.fullname))
-    }
-    // if (filterBy.price) {
-    //     gigs = gigs.filter(gig => gig.price <= filterBy.price)
-    // }
-    return gigs
+async function query(filterBy = { txt: '' }) {
+	var gigs = await storageService.query(STORAGE_KEY)
+	if (filterBy.txt) {
+		const regex = new RegExp(filterBy.txt, 'i')
+		gigs = gigs.filter(
+			(gig) =>
+				regex.test(gig.title) ||
+				regex.test(gig.description) ||
+				regex.test(gig.owner.fullname)
+		)
+	}
+	// if (filterBy.price) {
+	//     gigs = gigs.filter(gig => gig.price <= filterBy.price)
+	// }
+	return gigs
 }
 
 function getById(gigId) {
-    return storageService.get(STORAGE_KEY, gigId)
+	return storageService.get(STORAGE_KEY, gigId)
 }
 
 async function remove(gigId) {
-    await storageService.remove(STORAGE_KEY, gigId)
+	await storageService.remove(STORAGE_KEY, gigId)
 }
 
 async function save(gig) {
-    var savedGig
-    if (gig._id) {
-        savedGig = await storageService.put(STORAGE_KEY, gig)
-    } else {
-        // Later, owner is set by the backend
-        gig.owner = userService.getLoggedinUser()
-        savedGig = await storageService.post(STORAGE_KEY, gig)
-    }
-    return savedGig
+	var savedGig
+	if (gig._id) {
+		savedGig = await storageService.put(STORAGE_KEY, gig)
+	} else {
+		// Later, owner is set by the backend
+		gig.owner = userService.getLoggedinUser()
+		savedGig = await storageService.post(STORAGE_KEY, gig)
+	}
+	return savedGig
 }
 
 async function addGigMsg(gigId, txt) {
-    // Later, this is all done by the backend
-    const gig = await getById(gigId)
-    if (!gig.msgs) gig.msgs = []
+	// Later, this is all done by the backend
+	const gig = await getById(gigId)
+	if (!gig.msgs) gig.msgs = []
 
-    const msg = {
-        id: utilService.makeId(),
-        by: userService.getLoggedinUser(),
-        txt
-    }
-    gig.msgs.push(msg)
-    await storageService.put(STORAGE_KEY, gig)
+	const msg = {
+		id: utilService.makeId(),
+		by: userService.getLoggedinUser(),
+		txt,
+	}
+	gig.msgs.push(msg)
+	await storageService.put(STORAGE_KEY, gig)
 
-    return msg
+	return msg
 }
 
 function getEmptyGig() {
-    return {
-        _id: '',
-        title: '',
-        price: '',
-        owner: {},
-        daysToMake: null,
-        description: '',
-        imgUrls: [""],
-        tags: [],
-        likedByUsers: []
-    }
+	return {
+		_id: '',
+		title: '',
+		price: '',
+		owner: {},
+		daysToMake: null,
+		description: '',
+		imgUrls: [''],
+		tags: [],
+		likedByUsers: [],
+	}
 }
 
 // Initial data
-// ;(async ()=>{
-//     await storageService.post(STORAGE_KEY, { _id: "i101",title: "I will design your logo",price: 12,owner: {"_id": "u101","fullname": "Dudu Da","imgUrl": "url","level": "basic/premium","rate": 4},daysToMake: 3,description: "Make unique logo...", imgUrls: [""],tags: [ "logo-design", "artisitic",  "proffesional", "accessible" ],  likedByUsers: ['mini-user'] })
-//     await storageService.post(STORAGE_KEY, { _id: "i102",title: "I can do sprint 4 4 u!!",price: 220,owner: {"_id": "u102","fullname": "Dima","imgUrl": "url","level": "basic/premium","rate": 5},daysToMake: 10,description: "Pay me up and go have fun...", imgUrls: [""],tags: [ "artisitic",  "proffesional" ],  likedByUsers: ['mini-user'] })
-//     await storageService.post(STORAGE_KEY, { _id: "i103",title: "I make fake Coding Academy certifications",price: 190,owner: {"_id": "u102","fullname": "Dima","imgUrl": "url","level": "basic/premium","rate": 3},daysToMake: 1,description: "No one will ever know...", imgUrls: [""],tags: [ "artisitic",  "proffesional" ],  likedByUsers: ['mini-user'] })
+// ;(async () => {
+// 	await storageService.post(STORAGE_KEY, {
+// 		_id: 'i101',
+// 		title: 'I will design your logo',
+// 		price: 12,
+// 		owner: {
+// 			_id: 'u101',
+// 			fullname: 'Dudu Da',
+// 			imgUrl: 'url',
+// 			level: 'basic/premium',
+// 			rate: 4,
+// 		},
+// 		daysToMake: 3,
+// 		description: 'Make unique logo...',
+// 		imgUrls: [''],
+// 		tags: ['logo-design', 'artisitic', 'proffesional', 'accessible'],
+// 		likedByUsers: ['mini-user'],
+// 	})
+// 	await storageService.post(STORAGE_KEY, {
+// 		_id: 'i102',
+// 		title: 'I can do sprint 4 4 u!!',
+// 		price: 220,
+// 		owner: {
+// 			_id: 'u102',
+// 			fullname: 'Dima',
+// 			imgUrl: 'url',
+// 			level: 'basic/premium',
+// 			rate: 5,
+// 		},
+// 		daysToMake: 10,
+// 		description: 'Pay me up and go have fun...',
+// 		imgUrls: [''],
+// 		tags: ['artisitic', 'proffesional'],
+// 		likedByUsers: ['mini-user'],
+// 	})
+// 	await storageService.post(STORAGE_KEY, {
+// 		_id: 'i103',
+// 		title: 'I make fake Coding Academy certifications',
+// 		price: 190,
+// 		owner: {
+// 			_id: 'u102',
+// 			fullname: 'Dima',
+// 			imgUrl: 'url',
+// 			level: 'basic/premium',
+// 			rate: 3,
+// 		},
+// 		daysToMake: 1,
+// 		description: 'No one will ever know...',
+// 		imgUrls: [''],
+// 		tags: ['artisitic', 'proffesional'],
+// 		likedByUsers: ['mini-user'],
+// 	})
 // })()
-
-
 
 // const gig = {
 //     _id: "i101",
@@ -102,5 +151,5 @@ function getEmptyGig() {
 //     description: "Make unique logo...",
 //     imgUrls: [""],
 //     tags: [ "logo-design", "artisitic",  "proffesional", "accessible" ],
-//     likedByUsers: ['mini-user'] 
+//     likedByUsers: ['mini-user']
 //   }
--- src/views/explore/GigList.vue
+++ src/views/explore/GigList.vue
@@ -1,23 +1,28 @@
 <template>
-    <div class="gig-list-container">
-        <h2>Gigs</h2>
-        <ul class="gig-list">
-            <li v-for="gig in gigs" :key="gig._id">
-                <GigPreview :gig="gig">  
-                </GigPreview>
-            </li>
-        </ul>        
-    </div>
+	<div>
+		<h1>Gig List</h1>
+		<ul>
+			<li v-for="gig in gigs" :key="gig.id">
+				<h1>{{ gig }}</h1>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script>
 import GigPreview from './GigPreview.vue'
 
 export default {
-    props: ['gigs'],
-    components: {
-        GigPreview
-    }
+	computed: {
+		gigs() {
+			return this.$store.state.gigStore.gigs
+		},
+	},
+	components: {
+		GigPreview,
+	},
+	props: {
+		gigs: Array,
+	},
 }
-
 </script>
