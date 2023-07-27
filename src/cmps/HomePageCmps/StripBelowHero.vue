93a3eb9cb0744f44ea79255fb433a44e350dc35e
Gaash Shmilovich <gaash.shm@gmail.com>
2023-07-27 13:29:52 +0300

    adding homepage strip

---
+++ src/cmps/HomePageCmps/StripBelowHero.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="strip-container">
+    <span>Trusted by:</span>
+    <ul class="branding">
+      <li>
+        <img
+          src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/meta.12b5e5c.png"
+          alt="meta"
+        />
+      </li>
+      <li>
+        <img
+          src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/google.61e78c8.png"
+          alt="google"
+        />
+      </li>
+      <li>
+        <img
+          src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/netflix.96c5e3f.png"
+          alt="netflix"
+        />
+      </li>
+      <li>
+        <img
+          src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/pandg.0f4cfc2.png"
+          alt="p&g"
+        />
+      </li>
+      <li>
+        <img
+          src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/paypal.305e264.png"
+          alt="paypal"
+        />
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "strip",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  mounted() {},
+};
+</script>
