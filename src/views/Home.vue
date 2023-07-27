81e2a4a3bbf04809438ba3529ec9247f8bd73f79
Gaash Shmilovich <gaash.shm@gmail.com>
2023-07-27 13:31:54 +0300

    fixing homepage imports

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,7 @@
 <template>
   <section class="home-container">
-    <img
-      v-if="currentImage"
-      ref="rotatingImage"
-      alt="Vue logo"
-      :src="currentImage"
-      :key="currentImageIndex"
-      class="image-transition"
-    />
+    <Hero />
+    <StripBelowHero />
   </section>
 </template>
 
@@ -21,17 +15,7 @@ export default {
     StripBelowHero,
   },
   data() {
-    return {
-      images: [
-        // "https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto/v1/attachments/generic_asset/asset/1b6990afe0934244dda2c9aeed5de8d9-1674663021930/bg-hero-6-1792-x1.png",
-        "https://fiverr-res.cloudinary.com/image/upload/q_auto,f_auto/v1/attachments/generic_asset/asset/bb5958e41c91bb37f4afe2a318b71599-1599344049983/bg-hero-1-1792-x1.png",
-        "https://fiverr-res.cloudinary.com/image/upload/q_auto,f_auto/v1/attachments/generic_asset/asset/2413b8415dda9dbd7756d02cb87cd4b1-1599595203045/bg-hero-2-1792-x1.png",
-        "https://fiverr-res.cloudinary.com/image/upload/q_auto,f_auto/v1/attachments/generic_asset/asset/d14871e2d118f46db2c18ad882619ea8-1599835783966/bg-hero-3-1792-x1.png",
-        "https://fiverr-res.cloudinary.com/image/upload/q_auto,f_auto/v1/attachments/generic_asset/asset/93085acc959671e9e9e77f3ca8147f82-1599427734108/bg-hero-4-1792-x1.png",
-        "https://fiverr-res.cloudinary.com/image/upload/q_auto,f_auto/v1/attachments/generic_asset/asset/bb5958e41c91bb37f4afe2a318b71599-1599344049970/bg-hero-5-1792-x1.png",
-      ],
-      currentImageIndex: 0,
-    };
+    return {};
   },
   computed: {},
   methods: {},
