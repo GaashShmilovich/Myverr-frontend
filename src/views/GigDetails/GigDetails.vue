5ed192f39c7397d2c4bd4907ff12d27a229c01d5
Giora Marani <giora.marani@gmail.com>
2023-07-27 10:56:46 +0300

    in GigDetails removed getter of reviews

--- src/views/GigDetails/GigDetails.vue
+++ src/views/GigDetails/GigDetails.vue
@@ -4,23 +4,26 @@
       <!-- <h5 class="gig-category"> {{ filterby.category }}</h5> -->
       <h5 class="gig-category">/ logo-design / artisitic</h5>
       <h1 class="gig-title"> {{ gig.title }}</h1>
+
       <h5 class="gig-owner-details">
         <img :src="gig.owner.imgUrl" alt="">
         <p class="name">{{ gig.owner.fullname }}</p>
         <p class="instagram">{{ gig.owner.instagram }}</p>
         <p class="level">{{ gig.owner.level }} |</p>
+
         <section class="gig-owner-rate">
           <span v-for="i in gig.owner.rate || 1">
             <font-awesome-icon :style="{ color: '#ffb33e' }" icon="fa-solid fa-star" />
           </span>
         </section>
+
         <p class="rate-number" :style="{ color: '#ffb33e' }">{{ gig.owner.rate }} <span>(592)</span></p>
       </h5>
 
       <figure class="gig-gallery">
-        <!-- <img :src="gig.imgUrls" alt=""> -->
         <GigDetailsCarusela :gig="gig" />
       </figure>
+
       <h2>About this gig: </h2>
       <p> {{ gig.description }}</p>
 
@@ -29,8 +32,8 @@
 
     <div class="sticky-packages">
       <PackageType />
-
     </div>
+
   </section>
 
   <section v-else>
@@ -71,14 +74,10 @@ export default {
     },
   },
   computed: {
-    reviews() {
-      return this.$store.getters.reviews
-    },
   },
   components: {
     ReviewList,
     PackageType,
-    gigService,
     GigDetailsCarusela,
   },
 }
