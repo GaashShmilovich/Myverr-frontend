b6c860baa4f6c1c82697a960f74b1d5275c04d77
Oryan Hachamoff <oryanhach95@gmail.com>
2023-07-27 20:07:48 +0300

    added basic filtering by budget

--- src/views/explore/GigFilter.vue
+++ src/views/explore/GigFilter.vue
@@ -1,38 +1,72 @@
 <template>
 	<section class="filter-btns-container">
-		<button class="budget-btn filter-btn" @click="setFilter('budget')">
-			Budget <span></span>
-		</button>
-		<div v-if="filterBy === 'budget'" class="modal">
-			<p class="min-btn">Min.</p>
-			<input type="number" placeholder="Any" v-model="minBudget" />
-			<p class="max-btn">Max.</p>
-			<input type="number" placeholder="Any" v-model="maxBudget" />
-			<button class="clear-btn" @click="clearBudget">Clear All</button>
-			<button class="apply-btn" @click="applyBudget">Apply</button>
+		<!-- Budget Button and Modal -->
+		<div class="filter-group">
+			<button class="budget-btn filter-btn" @click="setFilter('budget')">
+				Budget <span></span>
+			</button>
+			<div v-if="filterBy === 'budget'" class="modal">
+				<div class="input-labels">
+					<p class="min-btn">MIN.</p>
+					<p class="max-btn">MAX.</p>
+				</div>
+				<div class="input-fields">
+					<input
+						type="number"
+						placeholder="Any"
+						v-model="minBudget"
+					/>
+					<input
+						type="number"
+						placeholder="Any"
+						v-model="maxBudget"
+					/>
+				</div>
+				<hr class="divider" />
+				<div class="actions">
+					<button class="clear-btn" @click="clearBudget">
+						Clear All
+					</button>
+					<button class="apply-btn" @click="applyBudget">
+						Apply
+					</button>
+				</div>
+			</div>
 		</div>
 
-		<button class="delivery-btn filter-btn" @click="setFilter('delivery')">
-			Delivery Time <span></span>
-		</button>
-		<div v-if="filterBy === 'delivery'" class="modal">
-			<label>
-				<input type="radio" value="24h" v-model="deliveryTime" />
-				Express 24h
-			</label>
-			<label>
-				<input type="radio" value="3days" v-model="deliveryTime" /> Up
-				to 3 days
-			</label>
-			<label>
-				<input type="radio" value="7days" v-model="deliveryTime" /> Up
-				to 7 days
-			</label>
-			<label>
-				<input type="radio" value="anytime" v-model="deliveryTime" />
-				Anytime
-			</label>
-			<button @click="applyDelivery">Apply</button>
+		<!-- Delivery Button and Modal -->
+		<div class="filter-group">
+			<button
+				class="delivery-btn filter-btn"
+				@click="setFilter('delivery')"
+			>
+				Delivery Time <span></span>
+			</button>
+			<div v-if="filterBy === 'delivery'" class="modal">
+				<label>
+					<input
+						type="radio"
+						value="24h"
+						v-model="deliveryTime"
+					/>Express 24h
+				</label>
+				<label>
+					<input type="radio" value="3days" v-model="deliveryTime" />
+					Up to 3 days
+				</label>
+				<label>
+					<input type="radio" value="7days" v-model="deliveryTime" />
+					Up to 7 days
+				</label>
+				<label>
+					<input
+						type="radio"
+						value="anytime"
+						v-model="deliveryTime"
+					/>Anytime
+				</label>
+				<button @click="applyDelivery">Apply</button>
+			</div>
 		</div>
 	</section>
 </template>
@@ -57,13 +91,17 @@ export default {
 		},
 		applyBudget() {
 			this.$emit('filterChanged', {
+				type: 'budget',
 				min: this.minBudget,
 				max: this.maxBudget,
 			})
 			this.filterBy = null
 		},
 		applyDelivery() {
-			this.$emit('filterChanged', { delivery: this.deliveryTime })
+			this.$emit('filterChanged', {
+				type: 'delivery',
+				delivery: this.deliveryTime,
+			})
 			this.filterBy = null
 		},
 	},
