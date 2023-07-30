3f9318ec05907f740a7b48fc31449a3cd63c6a57
Oryan Hachamoff <oryanhach95@gmail.com>
2023-07-30 21:13:31 +0300

    improved explore page layout

---
+++ src/cmps/ExploreCmps/ExploreHeader.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="explore-gig-category">
+		<i class="home-icon" v-html="$getSvg('home-icon')"></i>
+		<span class="breadcrumb-item">/</span>
+		<span class="breadcrumb-item">logo-design</span>
+		<span class="breadcrumb-item"></span>
+		<span class="breadcrumb-item last-category">artistic</span>
+	</div>
+</template>
+
+<script></script>
