de1739ac24679a30a5267fed508883abe06ba146
Gaash Shmilovich <gaash.shm@gmail.com>
2023-07-31 17:38:31 +0300

    finish homepage

--- src/cmps/AppFooter.vue
+++ src/cmps/AppFooter.vue
@@ -1,5 +1,21 @@
 <template>
-  <footer></footer>
+  <footer>
+    <div class="footer-container">
+      <div class="left">
+        <span class="logo">fiverr<span>&reg</span></span>
+        <p><span>&copy Fiverr International Ltd. 2023</span></p>
+      </div>
+      <div class="right">
+        <ul class="social">
+          <li><i class="search" v-html="$getSvg('twitter')"></i></li>
+          <li><i class="search" v-html="$getSvg('facebook')"></i></li>
+          <li><i class="search" v-html="$getSvg('linkedin')"></i></li>
+          <li><i class="search" v-html="$getSvg('pinterest')"></i></li>
+          <li><i class="search" v-html="$getSvg('instagram')"></i></li>
+        </ul>
+      </div>
+    </div>
+  </footer>
 </template>
 <script>
 export default {
--- src/cmps/HomePageCmps/Hero.vue
+++ src/cmps/HomePageCmps/Hero.vue
@@ -8,8 +8,33 @@
       :bullets="false"
       :arrows="false"
       fixed-height="680px"
+      :pause-on-hover="false"
     >
-      <vueper-slide v-for="(slide, i) in slides" :key="i" :image="slide.image">
+      <vueper-slide
+        v-for="(slide, i) in slides"
+        :key="i"
+        :image="slide.image"
+        :style="{ backgroundColor: slide.color }"
+      >
+        <template #content>
+          <div class="info">
+            <div class="info-pic">
+              <img :src="pic[i].content" alt="pic" />
+            </div>
+            <div class="info-txt">
+              <div class="fullname">
+                <span>
+                  {{ info.fullname[i] }}
+                </span>
+                <span class="stars">
+                  5
+                  <span class="svg" v-html="$getSvg('star')"></span>
+                </span>
+              </div>
+              <p class="expert">{{ info.expert[i] }}</p>
+            </div>
+          </div>
+        </template>
       </vueper-slide>
     </vueper-slides>
     <InsideHeroSearch />
@@ -33,45 +58,81 @@ export default {
         {
           image:
             "https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto,dpr_1.0/v1/attachments/generic_asset/asset/4637ac0b5e7bc7f247cd24c0ca9e36a3-1690384616487/jenny-2x.jpg",
+          color: "#0A4227",
         },
         {
           image:
             "https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto,dpr_1.0/v1/attachments/generic_asset/asset/4637ac0b5e7bc7f247cd24c0ca9e36a3-1690384616493/colin-2x.jpg",
+          color: "#A74459",
         },
         {
           image:
             "https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto,dpr_1.0/v1/attachments/generic_asset/asset/4637ac0b5e7bc7f247cd24c0ca9e36a3-1690384616487/scarlett-2x.jpg",
+          color: "#5F1629",
         },
         {
           image:
             "https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto,dpr_1.0/v1/attachments/generic_asset/asset/4637ac0b5e7bc7f247cd24c0ca9e36a3-1690384616493/jordan-2x.jpg",
+          color: "#104A26",
         },
         {
           image:
             "https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto,dpr_1.0/v1/attachments/generic_asset/asset/4637ac0b5e7bc7f247cd24c0ca9e36a3-1690384616497/christina-2x.jpg",
+          color: "#AE3905",
         },
       ],
-      currentImageIndex: 0,
+      pic: [
+        {
+          content:
+            "https://fiverr-res.cloudinary.com/q_auto,f_auto,w_40,dpr_1.0/v1/attachments/generic_asset/asset/7539ee9d7a6ab02e3d23069ebefb32c8-1690386499430/jenny-2x.png",
+        },
+        {
+          content:
+            "https://fiverr-res.cloudinary.com/q_auto,f_auto,w_40,dpr_1.0/v1/attachments/generic_asset/asset/7539ee9d7a6ab02e3d23069ebefb32c8-1690386499432/colin-2x.png",
+        },
+        {
+          content:
+            "https://fiverr-res.cloudinary.com/q_auto,f_auto,w_40,dpr_1.0/v1/attachments/generic_asset/asset/7539ee9d7a6ab02e3d23069ebefb32c8-1690386499428/scarlett-2x.png",
+        },
+        {
+          content:
+            "https://fiverr-res.cloudinary.com/q_auto,f_auto,w_40,dpr_1.0/v1/attachments/generic_asset/asset/7539ee9d7a6ab02e3d23069ebefb32c8-1690386499439/jordan-2x.png",
+        },
+        {
+          content:
+            "https://fiverr-res.cloudinary.com/q_auto,f_auto,w_40,dpr_1.0/v1/attachments/generic_asset/asset/7539ee9d7a6ab02e3d23069ebefb32c8-1690386499422/christina-2x.png",
+        },
+      ],
+      info: {
+        fullname: [
+          "Jenny",
+          "colinstark",
+          "Scarlett",
+          "jordanruncie_",
+          "Christina",
+        ],
+        expert: [
+          "Children's voice over",
+          "Creative director",
+          "Business founder",
+          "Production assistant",
+          "Jewellery shop owner",
+        ],
+      },
     };
   },
-  computed: {
-    currentImage() {
-      return this.images[this.currentImageIndex];
-    },
-  },
-  methods: {
-    rotateImages() {
-      this.currentImageIndex =
-        (this.currentImageIndex + 1) % this.images.length;
-    },
-  },
-  mounted() {
-    // setInterval(this.rotateImages, 3500);
-  },
+  computed: {},
+  methods: {},
 };
 </script>
 
 <style>
+.small-trans-div img {
+  display: none;
+}
+.small-trans-div img.active {
+  display: inline-block;
+}
 /* .thumbnails {
   margin: auto;
   max-width: 300px;
