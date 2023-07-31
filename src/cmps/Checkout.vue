8663eed5b9f9f150c7afee47f44d15983e1a4335
Giora Marani <giora.marani@gmail.com>
2023-07-31 17:41:43 +0300

    start designing payment page

---
+++ src/cmps/Checkout.vue
@@ -0,0 +1,7 @@
+<template>
+<h1>Checkout</h1>
+</template>
+
+<script>
+
+</script>
--- src/cmps/Payment.vue
+++ src/cmps/Payment.vue
@@ -1,66 +1,72 @@
 <template>
   <div class="payment">
 
+    <div>
     <section class="billing-info">
-      <p>Billing information</p>
-      <p>Your invoice will be issued according to the details listed here.</p>
-      <p>{{ user.username }}</p>
-      <p>{{ user.country }}</p>
+      <p class="title">Billing information</p>
+      <p class="txt">Your invoice will be issued according to the details listed here.</p>
+      <p><span class="name">{{ user.username }}</span> <p class="country">{{ user.country }}</p></p>
       <button>Add details</button>
     </section>
 
     <section class="payment-info">
-      <p>Payment Options</p>
+      <p class="title">Payment Options</p>
       <div>
-        <input @click="choosePayment('credit')" type="radio" id="credit" class="credit" name="payment" value="credit" checked>
+        <input @click="choosePayment('credit')" type="radio" id="credit" class="credit" name="payment" value="credit"
+          checked>
         <label for="credit">
-          Credit & Debit Cards
+          Credit & Debit Cards <i class="home-icon" v-html="$getSvg('credit-cards')"></i>
         </label>
-        <section class="credit-details hidden" :class="{'show' : paymentMethod === 'credit'}" >
 
-        <section class="card-number" >
-      <span>Card number</span>
-      <input type="text" v-model="cardNumber" />
-       </section>
-    <section class="expiration-date">
-      <span >Expiration date</span>
-      <input type="text" v-model="expiryDate" />
-    </section>
-    <section class="cvv">
-      <span>Security code</span>
-      <input type="text" v-model="cvv" />
-    </section>
-    <section class="first-name">
-      <span>First name</span>
-      <input type="text" v-model="firstName" />
-    </section>
-    <section class="last-name">
-      <span>Last name</span>
-      <input type="text" v-model="lastName" />
-    </section>
+        <section class="credit-details hidden" :class="{ 'show': paymentMethod === 'credit' }">
 
-  </section>
+          <section class="card-number">
+            <p>Card number</p>
+            <input type="text" v-model="cardNumber" />
+          </section>
+          <section class="expiration-date">
+            <p>Expiration date</p>
+            <input type="text" v-model="expiryDate" />
+          </section>
+          <section class="cvv">
+            <p>Security code</p>
+            <input type="text" v-model="cvv" />
+          </section>
+          <section class="first-name">
+            <p>First name</p>
+            <input type="text" v-model="firstName" />
+          </section>
+          <section class="last-name">
+            <p>Last name</p>
+            <input type="text" v-model="lastName" />
+          </section>
+
+        </section>
       </div>
 
 
       <div>
         <input @click="choosePayment('paypal')" type="radio" id="paypal" class="paypal" name="payment" value="paypal">
         <label for="paypal">
-          paypal
+          <i class="home-icon" v-html="$getSvg('paypal')"></i>
         </label>
       </div>
     </section>
 
+  </div>
+
+    <section class="checkout">
+      <Checkout />
+    </section>
 
 
-    
-    <RouterLink v-if="user" :to="'/user/' + user._id">Process Payment</RouterLink>
-    <RouterLink v-else="!user" to='/'>Process Payment</RouterLink>
+    <!-- <RouterLink v-if="user" :to="'/user/' + user._id">Process Payment</RouterLink> -->
+    <!-- <RouterLink v-else="!user" to='/'>Process Payment</RouterLink> -->
   </div>
 </template>
 
 <script>
-
+import Checkout from './Checkout.vue'
 import { userService } from '../services/user.service';
 export default {
   data() {
@@ -68,8 +74,8 @@ export default {
       cardNumber: '1234-5678-8765-4321',
       expiryDate: '08/08',
       cvv: '777',
-      firstName: '',
-      lastName: '',
+      firstName: 'John',
+      lastName: 'Brown',
       user: userService.getLoggedinUser(),
       paymentMethod: null,
     };
@@ -80,5 +86,8 @@ export default {
       console.log(this.paymentMethod);
     }
   },
+  components: {
+    Checkout,
+  }
 };
 </script>
