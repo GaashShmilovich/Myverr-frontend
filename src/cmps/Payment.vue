bb984c5482f3e921aa8da408c88d516758790186
Giora Marani <giora.marani@gmail.com>
2023-07-31 14:03:18 +0300

    start payment page

--- src/cmps/Payment.vue
+++ src/cmps/Payment.vue
@@ -1,24 +1,63 @@
 <template>
-    <div class="checkout">
-      <h2>Checkout Payment</h2>
-      <div class="form-group">
-        <label for="cardNumber">Card Number</label>
-        <input type="text" id="cardNumber" v-model="cardNumber" />
+  <div class="payment">
+
+    <section class="billing-info">
+      <p>Billing information</p>
+      <p>Your invoice will be issued according to the details listed here.</p>
+      <p>{{ user.username }}</p>
+      <p>{{ user.country }}</p>
+      <button>Add details</button>
+    </section>
+
+    <section class="payment-info">
+      <p>Payment Options</p>
+      <div>
+        <input @click="choosePayment('credit')" type="radio" id="credit" class="credit" name="payment" value="credit" checked>
+        <label for="credit">
+          Credit & Debit Cards
+        </label>
+        <section class="credit-details hidden" :class="{'show' : paymentMethod === 'credit'}" >
+
+        <section class="card-number" >
+      <span>Card number</span>
+      <input type="text" v-model="cardNumber" />
+       </section>
+    <section class="expiration-date">
+      <span >Expiration date</span>
+      <input type="text" v-model="expiryDate" />
+    </section>
+    <section class="cvv">
+      <span>Security code</span>
+      <input type="text" v-model="cvv" />
+    </section>
+    <section class="first-name">
+      <span>First name</span>
+      <input type="text" v-model="firstName" />
+    </section>
+    <section class="last-name">
+      <span>Last name</span>
+      <input type="text" v-model="lastName" />
+    </section>
+
+  </section>
       </div>
-      <div class="form-group">
-        <label for="expiryDate">Expiry Date</label>
-        <input type="text" id="expiryDate" v-model="expiryDate" />
-      </div>
-      <div class="form-group">
-        <label for="cvv">CVV</label>
-        <input type="text" id="cvv" v-model="cvv" />
-      </div>
-      <RouterLink v-if="user" :to="'/user/' + user._id" >Process Payment</RouterLink>
-      <RouterLink v-else="!user" to='/' >Process Payment</RouterLink>
-    </div>
 
 
-  </template>
+      <div>
+        <input @click="choosePayment('paypal')" type="radio" id="paypal" class="paypal" name="payment" value="paypal">
+        <label for="paypal">
+          paypal
+        </label>
+      </div>
+    </section>
+
+
+
+    
+    <RouterLink v-if="user" :to="'/user/' + user._id">Process Payment</RouterLink>
+    <RouterLink v-else="!user" to='/'>Process Payment</RouterLink>
+  </div>
+</template>
 
 <script>
 
@@ -29,8 +68,17 @@ export default {
       cardNumber: '1234-5678-8765-4321',
       expiryDate: '08/08',
       cvv: '777',
+      firstName: '',
+      lastName: '',
       user: userService.getLoggedinUser(),
+      paymentMethod: null,
     };
+  },
+  methods: {
+    choosePayment(method) {
+      this.paymentMethod = method
+      console.log(this.paymentMethod);
+    }
   },
 };
 </script>
--- src/services/user.service.local.js
+++ src/services/user.service.local.js
@@ -71,27 +71,29 @@ async function changeScore(by) {
 }
 
 function saveLocalUser(user) {
-    // user = {
-    //     id: user._id,
-    //     fullname: user.fullname,
-    //     imgUrl: user.imgUrl,
-    //     username: user.username,
-    //     password: user.password,
-    //     level: user.level,
-    //     reviews: [
-    //         {
-    //             id: user.reviews[0]._id,
-    //             gig: user.reviews[0].gig,
-    //             txt: user.reviews[0].txt,
-    //             rate: user.reviews[0].rate,
-    //             by: {
-    //                 _id: user.reviews[0].by._id,
-    //                 fullname: user.reviews[0].by.fullname,
-    //                 imgUrl: user.reviews[0].by.imgUrl
-    //             }
-    //         }
-    //     ],
-    // },
+    user = {
+        id: user._id,
+        isSeller: user.isSeller,
+        fullname: user.fullname,
+        imgUrl: user.imgUrl,
+        username: user.username,
+        password: user.password,
+        level: user.level,
+        country: user.country,
+        reviews: [
+            {
+                id: user.reviews[0]._id,
+                gig: user.reviews[0].gig,
+                txt: user.reviews[0].txt,
+                rate: user.reviews[0].rate,
+                by: {
+                    _id: user.reviews[0].by._id,
+                    fullname: user.reviews[0].by.fullname,
+                    imgUrl: user.reviews[0].by.imgUrl
+                }
+            }
+        ],
+    },
         sessionStorage.setItem(STORAGE_KEY_LOGGEDIN_USER, JSON.stringify(user))
     return user
 }
@@ -101,74 +103,81 @@ function getLoggedinUser() {
 }
 
 // Initial data
-// ; (async () => {
-//     await userService.signup({
-//         _id: "u101",
-//         fullname: "User 1",
-//         imgUrl: "/img/img1.jpg",
-//         username: "user1",
-//         password: "secret",
-//         level: "basic/premium",
-//         reviews: [
-//             {
-//                 id: "madeId",
-//                 gig: "{optional-mini-gig}",
-//                 txt: "Very kind and works fast",
-//                 rate: 4,
-//                 by: {
-//                     _id: "u102",
-//                     fullname: "user2",
-//                     imgUrl: "/img/img2.jpg"
-//                 }
-//             }
-//         ]
-//     }
 
-//     )
-//     await userService.signup({
-//         id: "u102",
-//         fullname: "User 2",
-//         imgUrl: "/img/img2.jpg",
-//         username: "user2",
-//         password: "topsecret",
-//         level: "premium",
-//         reviews: [
-//             {
-//                 id: "madeId",
-//                 gig: "{optional-mini-gig}",
-//                 txt: "Excellent work and great communication!",
-//                 rate: 5,
-//                 by: {
-//                     _id: "u103",
-//                     fullname: "User 3",
-//                     imgUrl: "/img/img3.jpg"
-//                 }
-//             }
-//         ]
-//     }
+; (async () => {
+    await userService.signup({
+        _id: "u101",
+        isSeller: true,
+        fullname: "User 1",
+        imgUrl: "/img/img1.jpg",
+        username: "user1",
+        password: "secret",
+        level: "basic/premium",
+        country: 'Israel',
+        reviews: [
+            {
+                id: "madeId",
+                gig: "{optional-mini-gig}",
+                txt: "Very kind and works fast",
+                rate: 4,
+                by: {
+                    _id: "u102",
+                    fullname: "user2",
+                    imgUrl: "/img/img2.jpg"
+                }
+            }
+        ]
+    }
 
-//     )
-//     await userService.signup({
-//         id: "u103",
-//         fullname: "User 3",
-//         imgUrl: "/img/img3.jpg",
-//         username: "user3",
-//         password: "hidden",
-//         level: "basic",
-//         reviews: [
-//             {
-//                 id: "madeId",
-//                 gig: "{optional-mini-gig}",
-//                 txt: "Great communication and quality work!",
-//                 rate: 5,
-//                 by: {
-//                     _id: "u101",
-//                     fullname: "User 1",
-//                     imgUrl: "/img/img1.jpg"
-//                 }
-//             }
-//         ]
-//     }
+    )
+    await userService.signup({
+        id: "u102",
+        isSeller: false,
+        fullname: "User 2",
+        imgUrl: "/img/img2.jpg",
+        username: "user2",
+        password: "topsecret",
+        level: "premium",
+        country: 'United States',
+        reviews: [
+            {
+                id: "madeId",
+                gig: "{optional-mini-gig}",
+                txt: "Excellent work and great communication!",
+                rate: 5,
+                by: {
+                    _id: "u103",
+                    fullname: "User 3",
+                    imgUrl: "/img/img3.jpg"
+                }
+            }
+        ]
+    }
 
-//     )
-// })()
+    )
+    await userService.signup({
+        id: "u103",
+        isSeller: false,
+        fullname: "User 3",
+        imgUrl: "/img/img3.jpg",
+        username: "user3",
+        password: "hidden",
+        level: "basic",
+        country: 'United Kingdom',
+        reviews: [
+            {
+                id: "madeId",
+                gig: "{optional-mini-gig}",
+                txt: "Great communication and quality work!",
+                rate: 5,
+                by: {
+                    _id: "u101",
+                    fullname: "User 1",
+                    imgUrl: "/img/img1.jpg"
+                }
+            }
+        ]
+    }
+
+    )
+})()
