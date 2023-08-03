838e7b89559904416c891325af94cf10efae2ac1
Giora Marani <giora.marani@gmail.com>
2023-08-03 17:08:39 +0300

    make changes in id of user

---
+++ src/cmps/BuyerProfile.vue
@@ -0,0 +1,151 @@
+<template>
+    <section class="dashboard-container">
+      <div class="background"></div>
+  
+      <div class="user-profile">
+        <div class="user-info">
+          <h1>{{ user.fullname }}</h1>
+          <h3>{{ user.country }}</h3>
+          <img :src="user.imgUrl" alt="" />
+          <button @click="doLogout">Logout</button>
+        </div>
+  
+        <div class="user-description">
+          <header class="desc-header">
+            <h1>Ready to earn on your own terms?</h1>
+          </header>
+          <div class="desc-content">
+            <button>Become a Seller</button>
+            <!-- <RouterLink>Become a Seller</RouterLink> -->
+          </div>
+        </div>
+      </div>
+      <div class="seller-section">
+        <div class="tabs">
+          <RouterLink
+            @click="setActiveTab"
+            :class="{ active: activeTab }"
+            :to="userGigsLink"
+            >My gigs</RouterLink
+          >
+          <!-- <RouterLink
+            @click="setActiveTab"
+            :class="{ active: activeTab }"
+            :to="userOrdersLink"
+            >My orders</RouterLink
+          > -->
+          <RouterLink
+            @click="setActiveTab"
+            :class="{ active: activeTab }"
+            :to="userOrdersLink"
+            >Orders</RouterLink
+          >
+          <!-- <RouterLink
+            @click="setActiveTab"
+            :class="{ active: activeTab }"
+            :to="userReviewsLink"
+            >Reviews</RouterLink
+          > -->
+        </div>
+        <div class="main-gigs">
+          <RouterView :user="user" />
+        </div>
+      </div>
+    </section>
+  </template>
+  
+  <script>
+  import { RouterLink, RouterView } from "vue-router";
+  import { showErrorMsg, showSuccessMsg } from "../../services/event-bus.service";
+  import { gigService } from "../../services/gig.service.local";
+  import { userService } from "../../services/user.service.local";
+  import {
+    getActionRemoveGig,
+    getActionUpdateGig,
+    getActionAddGigMsg,
+  } from "../../store/gig.store";
+  
+  export default {
+    props: {
+      user: Object,
+    },
+    data() {
+      return {
+        gigToAdd: gigService.getEmptyGig(),
+        activeTab: 0,
+      };
+    },
+    computed: {
+      userGigsLink() {
+        return `/user/${this.user._id}/gigs`;
+      },
+      userOrdersLink() {
+        return `/user/${this.user._id}/orders`;
+      },
+      userReviewsLink() {
+        return `/user/${this.user._id}/reviews`;
+      },
+      loggedInUser() {
+        return this.$store.getters.loggedinUser;
+      },
+      gigs() {
+        return this.$store.getters.gigs;
+      },
+    },
+    created() {
+      this.$store.dispatch({ type: "loadGigs" });
+    },
+    methods: {
+      doLogout() {
+        this.$store.dispatch({ type: "logout" });
+      },
+      async addGig() {
+        try {
+          await this.$store.dispatch({ type: "addGig", gig: this.gigToAdd });
+          showSuccessMsg("Gig added");
+          this.gigToAdd = gigService.getEmptyGig();
+        } catch (err) {
+          console.log(err);
+          showErrorMsg("Cannot add gig");
+        }
+      },
+      async removeGig(gigId) {
+        try {
+          await this.$store.dispatch(getActionRemoveGig(gigId));
+          showSuccessMsg("Gig removed");
+        } catch (err) {
+          console.log(err);
+          showErrorMsg("Cannot remove gig");
+        }
+      },
+      async updateGig(gig) {
+        try {
+          gig = { ...gig };
+          gig.price = +prompt("New price?", gig.price);
+          await this.$store.dispatch(getActionUpdateGig(gig));
+          showSuccessMsg("Gig updated");
+        } catch (err) {
+          console.log(err);
+          showErrorMsg("Cannot update gig");
+        }
+      },
+      async addGigMsg(gigId) {
+        try {
+          await this.$store.dispatch(getActionAddGigMsg(gigId));
+          showSuccessMsg("Gig msg added");
+        } catch (err) {
+          console.log(err);
+          showErrorMsg("Cannot add gig msg");
+        }
+      },
+      printGigToConsole(gig) {
+        console.log("Gig msgs:", gig.msgs);
+      },
+      setActiveTab(index) {
+        this.activeTab = index;
+      },
+    },
+    components: { RouterView, RouterLink },
+  };
+  </script>
+  
---
+++ src/cmps/CodeReview.vue
@@ -0,0 +1,54 @@
+<template></template>
+
+<script>
+export default {
+    methods: {
+        async addOrder() {
+            try {
+                const loggenInUser = await userService.getLoggedinUser()
+                const createdOrder = {
+                    createdAt: new Date(),
+                    buyer: {
+                        _id: loggenInUser._id,
+                        fullname: loggenInUser.fullname,
+                        username: loggenInUser.username,
+                        imgUrl: loggenInUser.imgUrl,
+                    },
+                    seller: {
+                        _id: this.gig.owner._id,
+                        fullname: this.gig.owner.fullname,
+                        imgUrl: this.gig.owner.imgUrl,
+                    },
+                    gig: {
+                        _id: this.gig._id,
+                        name: this.gig.title,
+                        imgUrls: this.gig.imgUrls,
+                        price: this.package.price
+                    },
+                    packageType: this.package.level,
+                    status: "pending",
+
+                }
+                await this.$store.dispatch({ type: 'addOrder', createdOrder })
+
+            } catch (err) {
+                console.error(err)
+            }
+
+        }, 
+    },
+    actions: {
+        async addOrder(context, { createdOrder }) {
+            try {
+                const newOrder = await orderService.save(createdOrder)
+                context.commit({ type: 'addOrder', newOrder })
+                return newOrder
+            } catch (err) {
+                console.log('orderStore: Error in addOrder', err)
+                throw err
+            }
+        }, 
+    },
+}
+
+</script>
