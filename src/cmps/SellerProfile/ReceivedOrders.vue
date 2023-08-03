684add2260b4a482e1d3cb17afade0f0ec586c1a
Gaash Shmilovich <gaash.shm@gmail.com>
2023-08-03 14:16:37 +0300

    adding reviews route

---
+++ src/cmps/SellerProfile/ReceivedOrders.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="orders-manager-container">
+    <div class="orders-stats">
+      <div>
+        <span>Annual Revenue</span>
+        <h3>$30</h3>
+      </div>
+      <div>
+        <span>Monthly Revenue</span>
+        <h3>$30</h3>
+      </div>
+      <div>
+        <span>Completed Orders </span>
+        <h3>1</h3>
+      </div>
+      <div>
+        <span>Pending Orders </span>
+        <h3>0</h3>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  methods: {},
+};
+</script>
---
+++ src/cmps/SellerProfile/SellerReviews.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <h1>hi</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  methods: {},
+};
+</script>
